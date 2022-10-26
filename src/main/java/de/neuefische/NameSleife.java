b1bea266dd69fcfb30ddb3feca3ee8c38b72c176
licetullmann <licetrosales@gmail.com>
2022-10-26 15:58:29 +0200

    Add Test writename1 in Klasse NameSleife

---
+++ src/main/java/de/neuefische/NameSleife.java
@@ -0,0 +1,8 @@
+package de.neuefische;
+
+public class NameSleife {
+    public static String writename1(){
+        String name = "Lis";
+        return name;
+    }
+}
---
+++ src/test/java/de/neuefische/NameSleifeTest.java
@@ -0,0 +1,21 @@
+package de.neuefische;
+
+import org.junit.jupiter.api.Test;
+import org.junit.platform.commons.annotation.Testable;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class NameSleifeTest {
+    @Test
+    void when_no_input_return_string(){
+    //GIVEN
+
+    String expected_result = "Lis";
+    //WHEN
+    String result = NameSleife.writename1();
+
+    //THEN
+    assertEquals(result, expected_result);
+}
+}
